f9adcdd30442410b86674cebcc01520137b6a4b2
gaowenshuo <90968450+gaowenshuo@users.noreply.github.com>
2022-05-29 20:20:19 +0800

    Update main.cpp

--- DEF2.0Code/main.cpp
+++ DEF2.0Code/main.cpp
@@ -6,6 +6,12 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     w.resize(1500,1200);
+    QFile qssFile("./theme.qss");
+    qssFile.open(QFile::ReadOnly);
+    QString qss = QLatin1String(qssFile.readAll());
+    w.setStyleSheet(qss);
+    qssFile.close();
+    w.setWindowIcon(QIcon("./icon.png"));
     w.show();
 
     return a.exec();
